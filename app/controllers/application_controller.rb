70263a125fdddba6fb1100c8a0db283ebb5337cd
bryn <thenazboz@gmail.com>
2020-04-15 17:24:28 +0100

    added 2 controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
-end
+    def secret
+        ENV["SECRET"]
+      end
+    
+      def generate_token(data)
+        JWT.encode(data, secret)
+      end
+    
+    
+      def decode_token
+        token = request.headers["Authorization"]
+        begin
+          JWT.decode(token, secret).first
+        rescue
+          {}
+        end
+      end
+    
+      def get_user
+        id = decode_token["id"]
+        User.find_by(id: id)
+      end
+    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,38 +3,19 @@ class UsersController < ApplicationController
     def sign_in
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
-            render json: { username: user.username, token: generate_token(id: user.id) }
+          render json: { username: user.username, token: generate_token(id: user.id) }
         else
-            render json: { error: "Username or Password is invalid "}
+          render json: { error: "Invalid Input"}
         end
-    end
-
-    def validate
-        if get_user
-            render json: { username: get_user.username, token: generate_token(id: get_user.id)}
-        else
-            render json: { error: "You are not authorized" }
-        end
-    end
-
-    def show
-        user = User.find(params[:id])
-        render json: user, serializer: UserSerializer, include: "*.*.*"
-    end
+      end
     
-    private
-
-    def user_params
-        params.require(:user).permit(:username,:password,:name,:surname,:email)
-    end
+      def validate
+        if get_user
+          render json: { username: get_user.username, token: generate_token(id: get_user.id)}
+        else
+          render json: { error: "Not authorized" }
+        end
+      end
+    
 
 end
-
-
-
-
-
-
-
-
-
